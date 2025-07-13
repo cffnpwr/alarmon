1650ddd7a316c31ce29006a1a3873f3459273374
default <cffnpwr@gmail.com>
2025-07-13 09:47:09 +0000

    feat ✨: Linux権限管理とエラーハンドリングの改善

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use color_eyre::Result;
+use anyhow::Result;
 use config::Config;
+use env_logger::Env;
+use log::error;
 use tokio::sync::mpsc;
 use tokio_util::sync::CancellationToken;
 use tui::models::UpdateMessage;
@@ -14,9 +16,15 @@ mod tui;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    #[cfg(target_os = "linux")]
+    prepare()?;
     #[cfg(all(debug_assertions, feature = "tokio-console"))]
     console_subscriber::init();
-    color_eyre::install()?;
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    color_eyre::install().map_err(|e| {
+        error!("Failed to install color_eyre: {e}");
+        anyhow::anyhow!("Failed to install color_eyre")
+    })?;
 
     let cli = Cli::parse();
     let config = Config::load(&cli.config)?;
@@ -25,31 +33,53 @@ async fn main() -> Result<()> {
     let (update_sender, update_receiver) = mpsc::channel::<UpdateMessage>(1000);
     let token = CancellationToken::new();
 
-    // 複数のタスクでconfigを使用するためクローンを作成
-    let config_for_ping = config.clone();
-    let config_for_tui = config.clone();
-
     // Ping監視タスクを起動
     let ping_token = token.clone();
+    let config_for_ping = config.clone();
     let ping_handle = tokio::spawn(async move {
         if let Err(e) = core::run_ping_monitoring(ping_token, &config_for_ping, update_sender).await
         {
-            eprintln!("Ping監視でエラーが発生しました: {e}");
+            let err_msg = format!("Error has occurred in ping monitoring: {e}");
+            ratatui::restore();
+            error!("{}", err_msg);
         }
     });
 
     // TUIタスクを起動
-    let tui_handle = tokio::spawn(async move {
-        if let Err(e) = tui::run_tui(token.clone(), update_receiver, &config_for_tui).await {
-            eprintln!("TUIでエラーが発生しました: {e}");
-        }
-    });
+    // let config_for_tui = config.clone();
+    // let tui_handle = tokio::spawn(async move {
+    //     if let Err(e) = tui::run_tui(token.clone(), update_receiver, &config_for_tui).await {
+    //         let err_msg = format!("Error has occurred in TUI: {e}");
+    //         ratatui::restore();
+    //         error!("{}", err_msg);
+    //     }
+    // });
 
     // どちらかのタスクが終了するまで待機
     tokio::select! {
         _ = ping_handle => {},
-        _ = tui_handle => {},
+        // _ = tui_handle => {},
     }
 
+    Ok(())
+}
+
+#[cfg(target_os = "linux")]
+fn prepare() -> anyhow::Result<()> {
+    use caps::{CapSet, Capability};
+    use sudo2::{running_as_root, running_as_suid};
+
+    if !(running_as_root() || running_as_suid())
+        && !caps::has_cap(None, CapSet::Permitted, Capability::CAP_NET_RAW)?
+    {
+        sudo2::escalate_with_env().map_err(|e| {
+            error!("Failed to escalate privileges: {e}");
+            anyhow::anyhow!("Failed to escalate privileges")
+        })?;
+    }
+
+    caps::set(None, CapSet::Permitted, &[Capability::CAP_NET_RAW].into())?;
+    caps::set(None, CapSet::Inheritable, &[Capability::CAP_NET_RAW].into())?;
+    caps::set(None, CapSet::Effective, &[Capability::CAP_NET_RAW].into())?;
     Ok(())
 }
